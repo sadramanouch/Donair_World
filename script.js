5630863214de4b85e4743a3f68cf0348e1bb2033
sadramanouch <sadra.manouchehri@gmail.com>
2022-06-29 17:46:16 -0700

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Wecome to Donair World!");
+let heading = document.getElementById("demo1");
+function myFunc1() {
+    demo1.style.backgroundColor = "red";
+    demo1.style.color = "white";
+}
+function myFunc2() {
+    demo1.style.backgroundColor = "#FEE1E1";
+    demo1.style.color = "black";
+}
